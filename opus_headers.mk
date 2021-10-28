234fe572d631b73badf9df0493840972aeb787ec
Ioannis Tsakpinis <iotsakp@gmail.com>
2021-10-28 21:59:33 +0300

    LWJGL CI Configuration

--- opus_headers.mk
+++ opus_headers.mk
@@ -6,4 +6,18 @@ src/opus_private.h \
 src/analysis.h \
 src/mapping_matrix.h \
 src/mlp.h \
-src/tansig_table.h
+src/tansig_table.h \
+libopusenc/include/opusenc.h \
+libopusenc/src/arch.h \
+libopusenc/src/ogg_packer.h \
+libopusenc/src/opus_header.h \
+libopusenc/src/picture.h \
+libopusenc/src/resample_sse.h \
+libopusenc/src/speex_resampler.h \
+libopusenc/src/unicode_support.h \
+ogg/include/ogg/ogg.h \
+ogg/include/ogg/os_types.h \
+ogg/src/crctable.h \
+opusfile/include/opusfile.h \
+opusfile/src/internal.h \
+opusfile/src/winerrno.h
